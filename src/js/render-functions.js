b87ac14392c1128904f3c4af924324f9b15dc2ce
Bohdan-Bondarenko <bogdan17raw@gmail.com>
2024-02-18 15:07:20 +0100

    fix6

---
+++ src/js/render-functions.js
@@ -0,0 +1,44 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryContainer, loadMoreBtn } from '../main.js';
+
+export function appendImages(images) {
+  const galleryItem = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<li class="gallery-item">
+              <a class="gallery-link" href="${largeImageURL}">
+                <img
+                  class="gallery-image"
+                  src="${webformatURL}"
+                  alt="${tags}"
+                />
+              </a>
+              <div class="image-details">
+                <p><b>Likes</b> ${likes}</p>
+                <p><b>Views</b> ${views}</p>
+                <p><b>Comments</b> ${comments}</p>
+                <p><b>Downloads</b> ${downloads}</p>
+              </div>
+           </li>`;
+      }
+    )
+    .join('');
+  galleryContainer.insertAdjacentHTML('beforeend', galleryItem);
+
+  let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+  });
+  gallery.refresh();
+
+  loadMoreBtn.style.display = images.length > 0 ? 'block' : 'none';
+}
